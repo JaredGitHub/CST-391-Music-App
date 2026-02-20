2cab7973c9da0df1e8db55de9d68e00f5add658d
Jared <45084265+JaredGitHub@users.noreply.github.com>
2026-02-20 15:02:52 -0700

    Music db on vercel

--- pages/api/db-check.ts
+++ pages/api/db-check.ts
@@ -1,18 +1,25 @@
-//pages/api/db-check.ts
-import type { NextApiRequest, NextApiResponse} from 'next';
+import type {NextApiRequest, NextApiResponse } from 'next';
+
 import {getPool} from '@/lib/db';
 
-const environment = process.env.NODE_ENV;
-const dbUrl = process.env.DATABASE_URL;
+const environment = process.env.NODE_ENV
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse)
-{
+const dbUrl = process.env.POSTGRES_URL ?? process.env.DATABASE_URL;
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
         const db = getPool();
-        const { rows } = await db.query('select now() as now');
-        res.status(200).json({ time: rows[0], message: `Jared's  Database connection successful. Running in ${environment}. DATABASE_URL: ${dbUrl}`});
-    }catch(err) {
+        let { rows } = await db.query('seelct now() as now');
+        const now = rows[0]?.now;
+
+        ({rows} = await db.query('SELECT artist FROM albums LIMIT 1'));
+        const artist = rows[0].artist;
+
+        res.status(200).json({time: now, artist: artist, message: `Jareds database conection successfull. Running in ${environment}. DATABASE_URL: ${dbUrl}`})
+
+    }catch (err) {
         res.status(500).json({error: 'Database connection failed',
-            details: (err as Error).message,message: `Jared's Database connection failed. Running in ${environment}. DATABASE_URL ${dbUrl}`});
+            details: (err as Error).message, message: `Jareds Database connection failed. RUnning in ${environment}. DATABASE_URL: ${dbUrl}`
+        })
     }
 }
